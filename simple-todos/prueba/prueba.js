2e41f3b21ff8ecdf6a66d8e3d8b8e98cfe1e8183
asu88 <asu.ramon88@gmail.com>
2014-11-27 14:17:17 +0100

    nueva edicion de prueba.js tanto en el servidor como en el cliente

--- simple-todos/prueba/prueba.js
+++ simple-todos/prueba/prueba.js
@@ -1,5 +1,15 @@
 if (Meteor.isClient) {
-  // counter starts at 0
+
+
+  /****
+     
+    Subscripcion de las lsitas o bases de datos creadas en el servidor
+  */
+   
+   Meteor.subscribe("UsersNick"); //Usersnicks referido a la lista de los usarios registrados en la aplicacion
+   
+ 
+ // counter starts at 0
   Session.setDefault("counter", 0);
 
   Template.hello.helpers({
@@ -8,18 +18,23 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.hello.events({
+  Template.hello.events({   
     'click button': function () {
       // Cuando hacen click aquí tenemos que crear una partida
       switch(startgame.num_players){
+          
+          //Parseamos la lista que tendremos con los jugadores en cola de inciar partida
+          // deacuerdo a lo que establezcamos
           case 2:
-
+                // Se inicia la partida con dos jugadores
           case 3:
-
+                //
           case 4:
 
           case 5:
-
+                
+          default:
+                // Mensaje "No puede iniciar partida de momento, intentelo mas tarde"
     }
   });
 }
@@ -27,18 +42,19 @@ if (Meteor.isClient) {
 Template.input.events = {
     'keydown input#chat_messages' : function (event){
         if (event.which == 13){
-            if(Meteor.us//Aqui tenemos que comprobar si el usuario esta autenticado
-                var usr_id= //Aqui buscamos el id de usuario
-                var message = $('#chat_message')
+            if(Meteor.userId()){    //Aqui tenemos que comprobar si el usuario esta autenticado
+                var usr_id= Meteor.user()._id; //Aqui buscamos el id de usuario
+                var message = $('#chat_message');
                 if (message.value.length == 0){
                     message.val('');
                 }else{
                     message.val('');
                     Messages.insert({
-                        usr_id:
-                        message:
-                        date:
-                        game_id:
+                        usr_id: usr_id,
+                        message: message.val(), 
+                        date: Date.now(),
+                        game_id
+                        
                     });
             }else{
                 alert("Error de autenticación");        
@@ -52,7 +68,16 @@ Template.input.events = {
 
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+ 
+
+  Meteor.publish("UsersNick", function() {
+    // publish only the field username of every user
+    return Meteor.users.find ({}, {fields: {username:1}});
+  });
+
+ 
+
+Meteor.startup(function () {
     // code to run on server at startup
   });
 }
