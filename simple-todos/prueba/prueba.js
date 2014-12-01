8de6efaf550c6b580a04d749e73f2b919b058abb
asu88 <asuakogo@epsilon17.aulas.gsyc.es>
2014-12-01 21:27:21 +0100

    colecciones añadidas al js

--- simple-todos/prueba/prueba.js
+++ simple-todos/prueba/prueba.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
     /****
-    Subscripcion de las lsitas o bases de datos creadas en el servidor
+    Subscripcion de las listas o bases de datos creadas en el servidor
     */
     Meteor.subscribe("usersNick"); //Usersnicks referido a la lista de los usarios registrados en la aplicacion
     Tracker.autorun(function(){
@@ -31,15 +31,19 @@ if (Meteor.isClient) {
         'click button': function () {
             // Cuando hacen click aquí tenemos que crear una partida
             match_name= parse($('#nombre').val());
+            var score= 0;
+            var status= 'waiting';
             num_players= parseInt($('#num_jugadores').val());
             difficulty= $('input[name=level]:checked', '#game_features').val();         
             $("#start_game").click(function() {
                     matches_game.insert({
-                        match_name: match_name;
-                        num_players: num_players;
-                        difficulty: difficulty;
-                        owner: Meteor.userId();                    
-                        owner_name: Meteor.user().username;
+                        match_name: match_name,
+                        num_players: num_players,
+                        difficulty: difficulty,
+                        owner: Meteor.userId(),                   
+                        owner_name: Meteor.user().username,
+                        score: score,
+                        status: status                        
                     });
             })        
         }
@@ -56,16 +60,17 @@ if (Meteor.isClient) {
             if (event.which == 13){
                 if(Meteor.us//Aqui tenemos que comprobar si el usuario esta autenticado
                     var usr_id= //Aqui buscamos el id de usuario
-                    var message = $('#chat_message')
+                    var message = $('#chat_message')                    
+                    var partida
                     if (message.value.length == 0){
                         message.val('');
                     }else{
                         message.val('');
                         Messages.insert({
-                            usr_id:
-                            message:
-                            date:
-                            game_id:
+                            usr_id:Meteor.userId(),     
+                            message:message.value,
+                            date: Date.now(),
+                            game_id: Session.get("current_game") // Pendiente
                         });
                 }else{
                     alert("Error de autenticación");        
@@ -80,10 +85,21 @@ if (Meteor.isClient) {
 
 
 if (Meteor.isServer) {
-    Meteor.publish("UsersNick", function() {
+    Meteor.publish('messages_game', function() {
         // publish only the field username of every user
-        return Meteor.users.find ({}, {fields: {username:1}});
+        return Messages_games.find ({}, {sort: {time:-1}});
+    });  //
+    
+    
+    Meteor.publish('matches_game', function() {
+        // publish only the field username of every user
+        return Matches_games.find ({}, {fields: {match_name:1;num_players:1;difficulty:1;owner:1;owner_name:1}});  
+    
+    //Meteor.publish("UsersNick", function() {
+        // publish only the field username of every user
+    //    return Meteor.users.find ({}, {fields: {username:1}});
     });
+    
     Meteor.startup(function () {
         // code to run on server at startup
     });
