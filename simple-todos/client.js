85a7941676055479e1a3760c8da25aa108356ec1
acarrioning <a.carrion.urjc@gmail.com>
2014-12-19 13:34:31 +0100

    nueva version

---
+++ simple-todos/client.js
@@ -0,0 +1,262 @@
+
+    /****
+    Subscripcion de las listas o bases de datos creadas en el servidor
+    **/
+     Meteor.subscribe("all_games"); 
+     Meteor.subscribe("users_data"); 
+     
+     var Busy = false;
+     var match_name;
+         
+     Tracker.autorun(function(){
+        var current_game = Session.get("current_game");
+        Meteor.subscribe("messages_current_game", current_game); // Mensajes del chat del juego
+        Meteor.subscribe("matches_game", current_game); // Marcador de las partidas
+             
+    });
+    
+    Meteor.startup(function(){
+        Session.set("current_game", "none");
+            $('#gamecontainer').hide();
+    			$('#container').hide();
+    			$('#gamecarcas').hide();
+    			$('#waiting_matches').hide();
+    			
+            $(document).on("click", ".alert .close", function(e) {
+            	$(this).parent().hide();       
+        });
+        $('#game_features').hide();
+        $('#waiting_for_players').hide();
+    });
+
+    var currentUser = null;
+		Tracker.autorun(function(){
+		console.log("current user: " + currentUser);
+		currentUser = Meteor.userId();
+		console.log("current user: " + currentUser);
+	});
+
+
+   Template.options.events({                 
+            'click #start_game': function () {
+		
+	        match_name=$('#nombre').val();
+		    var score= 0;
+		    var status= 'waiting';
+		    var usuid = Meteor.userId();
+		    var usu = Meteor.users.findOne(usuid);
+		   
+		    match_created = Matches_games.findOne({match_name:match_name});
+		    console.log(match_created);
+		    if (match_created != undefined){ 
+		    	    alert("The name of the match is already used, please, try other.");
+		    }else{
+			    n_players= parseInt($('input[name=n_players]:checked', '#game_features').val());   
+		            $('#game_features').hide();
+		            $('#display_matches').hide();
+		            $('waiting_for_players').show();
+
+		            Matches_games.insert({
+				id_user: usuid,
+		                match_name: match_name,
+		                num_players: n_players,
+		                //difficulty: difficulty,
+		                //owner: Meteor.userId(),                   
+		                //owner_name: Meteor.users().nick,
+		                //score: score,
+		                //status: status,
+		                //date: Date.now()                   
+		            });
+			    console.log(n_players);	
+			    console.log(match_name);
+			    Busy = true;
+		            //Meteor.call('add_player',match_name);
+                $('#waiting_for_players').show();
+		   }
+        },
+
+        'click #close_options': function () {
+                $('#game_features').hide();
+                $('#display_matches').show();
+                var game = Games.findOne({name:"Carcassonne"});
+		     		 Session.set("current_game", game._id);
+        },
+
+	    'click #wait_cancel': function () {
+                $('#waiting_for_players').hide();
+                $('#display_matches').show();
+
+		match_id = Matches_games.findOne({match_name:match_name});
+
+		console.log(match_id);
+		console.log(match_id._id);
+		Matches_games.remove(match_id._id)
+                alert("Su partida ha sido cancelada satisfactoriamente.");
+		Busy = false;
+        }
+    });
+                 
+      
+
+    Template.join_match.events({        
+        'click button': function (){
+            var array_players = [];
+            $("#join_match").click(function() {
+                            matches_game.insert({
+                                 array_player_names: array_players.push({name_player:Meteor.userId()})         
+                            });               
+            })               
+        }
+    });
+
+
+ //Nos falta la funcion que nos daran los de la i.a para enchufarles lo que se extraiga del array_player_names
+
+    // Para unirse a un juego (carcassone, alliensInvasion...)
+    // Template.join_game.events({
+       
+    //     'click button': function (){
+    //         $("#join_game").click(function() {
+    //                     games.insert({
+    //                             id_game:id_game,   // obtener el valor de id_game   
+    //                             game_type:game_type // obtener el game_type
+    //                     });   
+    //     			})
+    // 			}      
+    // });
+
+Template.menu_bar.events = {
+		 'click #AI_button': function () {
+		     if(!Busy){
+			     console.log("Template.menu_bar.events");
+			     $('#container').show();
+			     $('#gamecontainer').hide();
+			     $('#gamecarcas').hide();
+			     $('#waiting_matches').hide();
+			     var game = Games.findOne({name:"AlienInvasion"});
+			     Session.set("current_game", game._id);
+		     }
+		 },
+		 'click #FW_button': function () {
+		     if(!Busy){
+			     $('#container').hide();
+			     $('#gamecontainer').show();
+			     $('#gamecarcas').hide();
+			     $('#waiting_matches').hide();
+			     var game = Games.findOne({name:"FrootWars"});
+			     Session.set("current_game", game._id);
+		     }
+		 },
+		 'click #CC_button': function () {
+		     if(!Busy){
+			     $('#container').hide();
+			     $('#gamecontainer').hide();
+			     $('#gamecarcas').hide();
+			     $('#waiting_matches').show();
+			     $('#display_matches').show();
+			     $('#game_features').hide();
+			     var game = Games.findOne({name:"Carcassonne"});
+			     Session.set("current_game", game._id);
+		     }
+		 },
+		
+		 'click #New_G': function () {
+                     if(!Busy){
+		             $('#container').hide();
+		             $('#gamecontainer').hide();
+		             $('#game_features').show();
+		             $('#waiting_matches').show();
+                             $('#display_matches').hide();
+                             var game = Games.findOne({name:"Carcassonne"});
+		             Session.set("current_game", game._id);            
+	             }	
+                 }
+	}
+
+	Template.chat.none = function (){
+		 return Session.get("current_game") == "none";
+	}
+
+
+    /*
+    Template.draw_matches.events({   
+        var Match_owner = [];            
+        var Match_owners_Coll = Matches_games.find({status: 'waiting'}, {sort: { date:-1}});//lo primero que tenemos que hacer es                                                                                          extraer las ids de creadores de las 
+                                                        //partidas en waiting con esas ids relleno el resto de datos  | tengo que
+                                                        //extarer todos los campos con el find y luego ir extrayendo las ids de 
+                                                        //cada uno con un for each    
+        var Match_name = [];
+        var Match_players = [];
+        var Match_name = Matches_games.find({}, {sort: { date:-1}});//Rellenar bien
+        var Match_players = Matches_games.find({}, {sort: { date:-1}});//Rellenar bien            
+    });
+*/
+
+   Template.chat_messages.messages = function () {
+
+		 var messagesColl =  Messages_games.find({}, { sort: { time: -1 }});
+		 var messages = [];
+
+		 messagesColl.forEach(function(m){
+		 		console.log('traza1');
+		     var userName = Meteor.users.findOne(m.user_id).username;
+		     messages.push({name: userName , message: m.message});
+		     console.log('traza2');
+		 });
+
+    	return messages;
+	}
+    
+	Template.chat.gameName = function (){
+		 var game_id = Session.get("current_game");         
+		 if (game_id)
+              var game_name = Games.findOne({_id: game_id}).name; 
+         console.log("**************" + game_name);		 
+         return game_name;
+	}
+
+    
+    
+   // Mostrar la puntuación de cada jugador, a partir de la base de datos de users_data
+    // Template.players.players_points = function(){
+      
+    // 		 var users_data= Users_data.find ({}, {sort: {time:-1}});
+    // 		 var list_players = [];
+    // 		 users_data.forEach (function (u) {
+    // 		     var user = Meteor.users.findOne({_id:partida.usr_id});
+    // 		     if(user){
+    // 		         list_players.push({puntos: u.usr_score, player:u.nick}) // El         
+    // 		     }
+    // 		 });
+    
+    // 	 return list_players;    
+    //  }
+
+	Template.input.events = {
+		 'keydown input#message' : function (event) {
+		     if (event.which == 13) {
+		     console.log('has pulsado intro');
+		         if (Meteor.userId()){
+		             var user_id = Meteor.user()._id;
+		             var message = $('#message');
+		             if (message.value != '') {
+		             	console.log('guardo el mensaje');
+		                 Messages_games.insert({
+		                     user_id: user_id,
+		                     message: message.val(),
+		                     time: Date.now(),
+		                     game_id: Session.get("current_game")
+		                 });
+		                 message.val('')
+		             }
+		         }
+		         else {
+		             $("#login-error").show();
+		         }
+		     }
+		 } 
+	}
+	
+   Accounts.ui.config({
+             passwordSignupFields: "USERNAME_ONLY"
+   });
